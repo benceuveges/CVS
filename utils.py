96b7f40b1526b1ce2141cf094c63a6ccdb6cc03f
benceuveges <103946807+benceuveges@users.noreply.github.com>
2022-05-23 00:46:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -64,15 +64,15 @@ def check_accuracy(loader, model, device="cuda"):
             y = y.to(device).unsqueeze(1)
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
-            #num_correct += (preds == y).sum()
-            #num_pixels += torch.numel(preds)
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
             dice_score += (2 * (preds * y).sum()) / (
                 (preds + y).sum() + 1e-8
             )
 
-    #print(
-       # f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
-    #)
+    print(
+        f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+    )
     print(f"Dice score: {dice_score/len(loader)}")
     model.train()
 
